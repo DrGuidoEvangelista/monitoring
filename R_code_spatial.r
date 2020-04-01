67d6271949427cd06eba0bd4ff54ffa30b8f88d8
DrGuidoEvangelista <62661697+DrGuidoEvangelista@users.noreply.github.com>
2020-04-01 17:57:10 +0200

    Create R_code_spatial.r

---
+++ R_code_spatial.r
@@ -0,0 +1,91 @@
+# R code for spatial view of points
+install.packages("sp")
+library(sp)
+
+data(meuse)
+
+head(meuse)
+
+#coordinates
+coordinates(meuse) = ~x+y
+plot(meuse)
+
+spplot(meuse,"zinc")
+
+spplot(meuse,"cadmium")
+
+#Exercise : plot the spatial amount of copper
+spplot(meuse, "copper",main="copper concentration")
+
+#bubble are big if the parameter of zinc or whatever is big
+bubble(meuse, "zinc")
+bubble(meuse, "zinc", main="zinc concentration")
+
+#exercise : bubble copper in read
+
+bubble(meuse,"copper",main="copper concentration", col="red")
+
+
+#importing new data downloaded from iol and put it in a folder called lab in :C 
+#setting the working directory: lab
+#Windows users
+setwd("C:/lab")
+
+#now we can import data
+
+#name of the dataset = covid
+#heat =T means true,correct way to see the data --> you can notice that in the first line there are no datas
+covid <- read.table("covid_agg.csv", head=T
+head(covid)
+
+attach(covid)
+plot(country,cases)
+
+plot(country,cases, las=0) #parallel labels
+plot(country,cases, las=1) #orizzontal labels
+plot(country,cases, las=2) #perpendicular labels
+plot(country,cases, las=3) #vertical labels
+plot(country,cases, las=3, cex.axis=0.5)  #cex = exagerating
+
+#ggplot2 package
+install.packages("ggplot2")
+library(ggplot2)  #require(ggplot2) 
+
+# type q() to save the data and close
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
