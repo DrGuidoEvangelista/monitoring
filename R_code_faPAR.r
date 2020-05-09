03575126f16873118869fd79c870bba4c19c84ca
DrGuidoEvangelista <62661697+DrGuidoEvangelista@users.noreply.github.com>
2020-05-09 11:23:04 +0200

    Create R_code_faPAR.r

---
+++ R_code_faPAR.r
@@ -0,0 +1,23 @@
+  #How to look to chemical cycles by satellites
+  library(raster)
+  library(rasterVis)
+  library(rasterdiv)
+  
+  plot(copNDVI)    #copernicus and NDVI
+ 
+copNDVI <- reclassify(copNDVI, cbind(253:255, NA))
+
+levelplot(copNDVI)
+ 
+setwd("C:/lab/") 
+faPAR10 <- raster("faPAR10.tif")
+levelplot(faPAR10)
+
+#to save the pic as a pdf - IMPORTANT
+pdf("copNDVI.pdf")
+levelplot(copNDVI)
+dev.off()
+
+pdf("faPAR10.pdf")
+levelplot(faPAR10)
+dev.off()
