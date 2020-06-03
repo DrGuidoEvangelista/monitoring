f98a94ae7830806c3282d8fb33eaa44b97075268
DrGuidoEvangelista <62661697+DrGuidoEvangelista@users.noreply.github.com>
2020-06-03 17:58:21 +0200

    Create R_code_NO2.r

---
+++ R_code_NO2.r
@@ -0,0 +1,6 @@
+#Analysis on changing of NO2 concentration in time
+#Donwload the EN zip file from iol and extract it into the lab foulder
+#put the images into a new foulder called "NO2" inside lab
+
+setwd("C:/lab/NO2/")
+
